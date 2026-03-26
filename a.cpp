634d12a85203408b2ff48ca27f03e3055b49d9e1
ad-digital-labs <ad.digitallabs@atomicmail.io>
2026-03-26 19:13:08 +0800

    proto

---
+++ a.cpp
@@ -0,0 +1,47 @@
+#define P_OK 0
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+std::string segment[12]
+{
+    " _ | ||_|",
+    " _  _||_ ",
+    " _  _| _|",
+    " _  _| _|",
+    "   | |  |",
+    " _ | _| _|",
+    " _ | ||_|",
+    " _  _|  |",
+    " _ | ||_|",
+    " _ | _| _|"
+};
+
+int display(std::string str)
+{
+
+    int res;
+
+    std::string ln01=0;
+    std::string ln02=0;
+    std::string ln03=0;
+
+
+    
+
+
+
+
+    res=P_OK;
+
+    return res;
+
+};
+
+
+
+
